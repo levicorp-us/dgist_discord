04e2919ef1aac26613552c932e6d4529ed72a580
levicorp-us <levicorpus02@gmail.com>
2023-09-22 05:36:47 +0000

    fix bugs

--- main_bot/__main__.py
+++ main_bot/__main__.py
@@ -2,19 +2,18 @@ import os
 import lightbulb
 import hikari
 
-from main_bot import GUILD_ID, _token
-
-bot = lightbulb.BotApp(
+if __name__ == "__main__":
+    from main_bot import GUILD_ID, _token
+    bot = lightbulb.BotApp(
     token = _token,
     prefix="!",
     default_enabled_guilds=GUILD_ID,
     intents = hikari.Intents.ALL
-)
+    )
 
-# bot.load_extensions('extensions.example') # specific extensions available
-bot.load_extensions_from('./extensions') # add extensions in the dir
-
-if __name__ == "__main__":
+    # bot.load_extensions('extensions.example') # specific extensions available
+    bot.load_extensions_from('./extensions') # add extensions in the dir
+    
     if os.name != "nt":
         import uvloop
         uvloop.install()
